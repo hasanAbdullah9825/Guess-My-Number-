6f914084684b94952ef8222cd5efa61c9d2e9700
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-02-09 22:52:28 +0600

    dom practice

--- script.js
+++ script.js
@@ -7,12 +7,45 @@
 // document.querySelector('.score').textContent=10;
 // document.querySelector('.guess').value=23;
 
-document.querySelector('.check').addEventListener('click',function(){
+const secreatNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secreatNumber;
+document.querySelector('.check').addEventListener('click', function () {
 
-    const guess =Number(document.querySelector('.guess'));
+    const guess = Number(document.querySelector('.guess').value);
 
-    if(!guess){
 
-        document.querySelector('.message').textContent='No number given';
+    if (!guess) {
+
+        document.querySelector('.message').textContent = 'No number given';
+
+    }
+    else if (guess === secreatNumber) {
+        document.querySelector('.message').textContent = "yee! Correct number";
+
+    }
+    else if (guess > secreatNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = "Too Big!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = "You lost The game";
+            document.querySelector('.score').textContent = 0;
+        }
+
+    }
+    else if (guess < secreatNumber) {
+        if (score > 1) {
+            document.querySelector('.message').textContent = "Too small!";
+            score--;
+            document.querySelector('.score').textContent = score;
+        }
+        else {
+            document.querySelector('.message').textContent = "You lost The game";
+            document.querySelector('.score').textContent = 0;
+        }
+
     }
 })
