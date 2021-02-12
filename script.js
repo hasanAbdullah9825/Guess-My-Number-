4fbe2112238696d23a3f57901283ecffba0c3d9d
hasanAbdullah9825 <hasan.techtomorrow@gmail.com>
2021-02-12 16:52:20 +0600

    Again play the game

--- script.js
+++ script.js
@@ -7,10 +7,14 @@
 // document.querySelector('.score').textContent=10;
 // document.querySelector('.guess').value=23;
 
-const secreatNumber = Math.trunc(Math.random() * 20) + 1;
+
+
+let secreatNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secreatNumber;
-document.querySelector('.check').addEventListener('click', function () {
+let highScore=0;
+// document.querySelector('.number').textContent = secreatNumber;
+
+const game = function () {
 
     const guess = Number(document.querySelector('.guess').value);
 
@@ -22,8 +26,13 @@ document.querySelector('.check').addEventListener('click', function () {
     }
     else if (guess === secreatNumber) {
         document.querySelector('.message').textContent = "yee! Correct number";
-        document.querySelector('body').style.backgroundColor='green';
-        document.querySelector('.number').style.width='30rem';
+        document.querySelector('body').style.backgroundColor = 'green';
+        document.querySelector('.number').style.width = '30rem';
+
+        if(score>highScore){
+            highScore=score;
+            document.querySelector('.highscore').textContent=highScore;
+        }
 
     }
     else if (guess > secreatNumber) {
@@ -50,4 +59,19 @@ document.querySelector('.check').addEventListener('click', function () {
         }
 
     }
-})
+
+
+}
+
+document.querySelector('.check').addEventListener('click', game);
+document.querySelector('.again').addEventListener('click', function () {
+    secreatNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector('.score').textContent = 20;
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+   
+    game();
+
+});
